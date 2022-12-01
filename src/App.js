0e9c5dcac664775800d16a59f67242d252834fff
shivam <shivamt2023@gmail.com>
2022-12-02 00:54:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from 'axios'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+function App(){
+    const[photo , setPhoto] = useState("");
+ const [result , setresult] = useState([])
+
+    const SearchImg = () =>{
+        axios.get(`https://api.unsplash.com/search/photos?page=1&query=${photo}&client_id=GrLthNqqcnWLrEsOwC3D8XoOaxEHHtdkeyRfjF6cQ0I`)
+        .then((Response)=>{
+         
+            setresult(Response.data.results)
+        })
+    }
+
+
+
+    return(
+        <>
+        <div className='container'>
+          <h1>Live Photo Search</h1>
+        
+          <input type="text"  value={photo} onChange={(e) => {
+                        setPhoto(e.target.value)
+                    }} placeholder="Search Free Image"/>
+          <button type='submit' onClick={SearchImg}>Search</button>
+        </div>
+    
+    
+        
+        <div className="container_2">
+                    <div className='row'>
+                        {result.map((value) => {
+                            return (
+                                <div className='col'>
+                                        <img src={value.urls.small} alt='' />
+                                </div>
+                            )
+                        })}
+                    </div>
+                  
+        </div>
+        </>
+      )
+    }
+    
+
 
 export default App;
